30c8513272708af3a7c4caca09af9c18220909ba
zakareaaa <zakrea.alkashef@gmail.com>
2026-02-26 02:23:31 +0300

    Backend Setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express();
+const PORT = 5002
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.listen(PORT,() => {
+  console.log(`Server running on http://localhost:${PORT}`);
+})
+
