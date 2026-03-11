03b8f207caaf37543501e233f0d4f58f7d57676b
zakareaaa <zakrea.alkashef@gmail.com>
2026-03-11 21:08:07 +0300

    feat(auth): add basic register and login functionality

---
+++ backend/controllers/authController.js
@@ -0,0 +1,58 @@
+import prisma from '../prisma/client.js'
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
+
+export const register = async (req, res) => {
+  try{
+    const { firstName, lastName, email, password, role, phoneNumber } = req.body;
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    const user = await prisma.user.create({
+      data:{
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        password:hashedPassword,
+        role
+      }
+    });
+
+    res.json(user);
+
+  }catch(error){
+    res.status(500).json({error: error.message})
+  }
+};
+
+export const login = async (req,res)=>{
+  try {
+
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email }
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({ token });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
