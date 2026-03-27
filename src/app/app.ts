ac456b09e345234ff446d632d7edfebc879ffd3f
Sama Alotaiby <samaotaiby2004@gmail.com>
2026-03-28 01:35:09 +0300

    student dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,36 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { RouterOutlet, RouterModule, Router, NavigationEnd } from '@angular/router';
 import { Reveal } from './Directives/reveal';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, Reveal],
+  imports: [RouterOutlet, RouterModule, Reveal, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('InternPath');
+
+  isLoggedIn = false;
+  userName = 'Sama';
+  userInitial = 'S';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const loggedInRoutes = [
+          '/student-dashboard',
+          '/student-settings',
+          '/profile-setup',
+          '/internships',
+          '/recruiter-dashboard'
+        ];
+        this.isLoggedIn = loggedInRoutes.some(route =>
+          event.urlAfterRedirects.startsWith(route)
+        );
+      }
+    });
+  }
 }
