013ccba8fbac337ee47b9c2dc566368f81318a11
zakareaaa <zakrea.alkashef@gmail.com>
2026-03-28 10:50:13 +0300

    rest of student routes and controllers

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,13 +1,14 @@
 import express from "express"
 import authenticate from "../middleware/authenticate.js"
 import authorizeRole from "../middleware/authorizeRole.js"
-import { getStudentProfile, updateStudentProfile } from "../controllers/studentController.js"
+import { getStudentProfile, updateStudentProfile,getStudentCV } from "../controllers/studentController.js"
 
 const router = express.Router()
 
 
 
-router.get("/profile", authenticate, authorizeRole(1), getStudentProfile)
-router.put("/profile", authenticate, authorizeRole(1), updateStudentProfile)
+router.get("/profile", authenticate, authorizeRole(0), getStudentProfile)
+router.put("/profile", authenticate, authorizeRole(0), updateStudentProfile)
+router.get("/cv", authenticate, authorizeRole(0), getStudentCV)
 
 export default router
